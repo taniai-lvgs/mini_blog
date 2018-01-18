981468761510d2847f328ef6dcc848ac41f63bcc
taniai-lvgs <keisuke.taniai@leverages.jp>
2018-01-19 08:17:29 +0900

    signupページの作成

--- html/application/views/account/signup.php
+++ html/application/views/account/signup.php
@@ -5,18 +5,28 @@
 <form action="<?PHP echo $base_url; ?>/account/register" method="post">
     <input type="hidden" name="_token" value="<?PHP echo $this->escape($_token); ?>" />
 
+    <?php if (isset($errors) && count($errors) > 0): ?>
+    <ul class="error_list">
+        <?php foreach ($errors as $error): ?>
+        <li><?php echo $this->escape($error); ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <?php endif; ?>
+
     <table>
         <tbody>
             <tr>
                 <th>ユーザーID</th>
                 <td>
-                    <input type="text" name="user_name" value="" />
+                    <input type="text" name="user_name"
+                           value="<?php echo $this->escape($user_name); ?>" />
                 </td>
             </tr>
             <tr>
                 <th>パスワード</th>
                 <td>
-                    <input type="password" name="password" value="" />
+                    <input type="password" name="password"
+                           value="<?php echo $this->escape($password); ?>" />
                 </td>
             </tr>
         </tbody>
